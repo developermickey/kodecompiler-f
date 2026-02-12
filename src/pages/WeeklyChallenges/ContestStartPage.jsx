ab80e8956da74180b398ca3f05ea5684f0a98bb5
Valtryek <amank225566@gmail.com>
2026-02-12 19:35:47 +0530

    Bug-fix
    Signed-off-by: Aman Ahmad <amank225566@gmail.com>

--- src/pages/WeeklyChallenges/ContestStartPage.jsx
+++ src/pages/WeeklyChallenges/ContestStartPage.jsx
@@ -74,7 +74,7 @@ export default function ContestStartPage() {
       }
     };
     if (id) fetchContestData();
-  }, [id]);
+  }, [id, user]);
 
   useEffect(() => {
     const fetchContestleaderboard = async () => {
